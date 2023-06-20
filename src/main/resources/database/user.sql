d9412bcc8e0749d944344582b7e83095e8c4af1e
zy_xdu <zy1231888@126.com>
2023-06-20 14:08:48 +0800

    update

--- src/main/resources/database/user.sql
+++ src/main/resources/database/user.sql
@@ -4,8 +4,6 @@ create table user(
     number varchar(20) primary key not null ,
     password varchar(20) not null
 );
-insert into user values (11111,11111);
-insert into user values (22222,22222);
-insert into user values (33333,33333);
-insert into user values (44444,44444);
-insert into user values (55555,55555);
+insert into user values (20009200512,20009200512);
+insert into user values (20009200692,20009200692);
+insert into user values (20009201245,20009201245);
